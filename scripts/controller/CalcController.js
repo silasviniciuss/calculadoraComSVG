e53050f64508e71d6b86e23805868754bd349352
silasviniciuss <silasvinicius.dev@gmail.com>
2022-12-06 22:19:00 -0300

    Criando o controller

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,35 @@
 class CalcController{
 
-    
+    constructor (){
 
+        this._displayCalc = "0";
+        this.currentDate;
+
+    }
+
+
+    get displayCalc(){
+
+        return this._displayCalc;
+
+    };
+
+    set displayCalc(valor){
+
+        this._displayCalc = valor;
+
+    }
+
+    get dataAtual(){
+
+        return this.currentDate;
+
+    }
+
+    set dataAtual(data){
+
+        this.currentDate = valor;
+
+    }
 
 }
